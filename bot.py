f17966b0920216994302d8d7f469ce10723bfcbe
devid-saakyan <59139074+devid-saakyan@users.noreply.github.com>
2019-12-23 14:29:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,14 +18,14 @@ def send_anything(message):
     chat_id = message.chat.id
     if message.text == '🏒🥅 Hockey (USA NXL 2019 - 2020)  🏒🥅':
         text1 = 'Лови список матчей по хоккею'
-        bot.send_message(chat_id, text1)
         bot.send_message(chat_id, '👇👇Введи числом количество матчей для показа (ИХ ОКОЛО 500) 👇👇')
+        bot.send_message(chat_id, text1)
         bot.register_next_step_handler(message, obrabotka, ans_h)
     elif message.text == '🏀🏀 Basketball (VTB - United - League 2019-2020) 🏀🏀':
         text2 = 'Лови список мачтей по баскетболу'
+        bot.send_message(chat_id, '👇👇Введи числом количество матчей для показа (ИХ ОКОЛО 500) 👇👇')
         bot.send_message(chat_id, text2)
-        for i in range(len(ans_b)):
-            bot.send_message(chat_id, '🧐🧐   🏀{}🏀   🆚🆚  🏀{}🏀   🧐🧐   ⏰ {} ⏰'.format(ans_b[i][0], ans_b[i][1], ans_b[i][2]))
+        bot.register_next_step_handler(message, obrabotka2, ans_h)
     elif message.text == '❌STOP❌':
         bot.send_message(chat_id, '❌❌Бот остановлен❌❌')
         bot.stop_polling()
@@ -34,8 +34,15 @@ def obrabotka(message, data):
     chat_id = message.chat.id
     if message.text.isdigit():
         for i in range(int(message.text)):
-            bot.send_message(chat_id, ' 😱😱   🥅{}🥅 🆚🆚 🥅{}🥅  😱😱  ⏰ {} ⏰'.format(ans_h[i][0],ans_h[i][1], ans_h[i][2]))
-                         
+            bot.send_message(chat_id, ' 😱😱   🥅{}🥅 🆚🆚 🥅{}🥅  😱😱  ⏰ {} ⏰'.format(ans_h[i][0],ans_h[i][1], ans_h[i][2]), reply_markup = keyboard())
+    bot.register_next_step_handler(message, send_anything)
+
+def obrabotka2(message, data):
+    chat_id = message.chat.id
+    if message.text.isdigit():
+        for i in range(int(message.text)):
+            bot.send_message(chat_id, ' 🧐🧐   🏀{}🏀   🆚🆚  🏀{}🏀   🧐🧐   ⏰ {} ⏰'.format(ans_b[i][0], ans_b[i][1], ans_b[i][2]))
+    bot.register_next_step_handler(message, send_anything)
                          
 def keyboard():
     markup = types.ReplyKeyboardMarkup(one_time_keyboard = True, resize_keyboard = True)
