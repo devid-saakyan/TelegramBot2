f2e10af4a424e8df602862a7f291b300bac88732
devid-saakyan <59139074+devid-saakyan@users.noreply.github.com>
2019-12-23 14:37:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,12 +17,10 @@ def send_welcome(message):
 def send_anything(message):
     chat_id = message.chat.id
     if message.text == '🏒🥅 Hockey (USA NXL 2019 - 2020)  🏒🥅':
-        text1 = 'Лови список матчей по хоккею'
         bot.send_message(chat_id, '👇👇Введи числом количество матчей для показа (ИХ ОКОЛО 500) 👇👇')
         bot.register_next_step_handler(message, obrabotka, ans_h)
         bot.send_message(chat_id, text1)
     elif message.text == '🏀🏀 Basketball (VTB - United - League 2019-2020) 🏀🏀':
-        text2 = 'Лови список мачтей по баскетболу'
         bot.send_message(chat_id, '👇👇Введи числом количество матчей для показа (ИХ ОКОЛО 500) 👇👇')
         bot.register_next_step_handler(message, obrabotka2, ans_h)
         bot.send_message(chat_id, text2)
@@ -32,14 +30,18 @@ def send_anything(message):
 
 def obrabotka(message, data):
     chat_id = message.chat.id
+    text1 = 'Лови список матчей по хоккею'
     if message.text.isdigit():
+        bot.send_message(chat_id, text1)
         for i in range(int(message.text)):
             bot.send_message(chat_id, ' 😱😱   🥅{}🥅 🆚🆚 🥅{}🥅  😱😱  ⏰ {} ⏰'.format(ans_h[i][0],ans_h[i][1], ans_h[i][2]), reply_markup = keyboard())
     bot.register_next_step_handler(message, send_anything)
 
 def obrabotka2(message, data):
     chat_id = message.chat.id
+    text2 = 'Лови список мачтей по баскетболу'
     if message.text.isdigit():
+        bot.send_message(chat_id, text2)
         for i in range(int(message.text)):
             bot.send_message(chat_id, ' 🧐🧐   🏀{}🏀   🆚🆚  🏀{}🏀   🧐🧐   ⏰ {} ⏰'.format(ans_b[i][0], ans_b[i][1], ans_b[i][2]))
     bot.register_next_step_handler(message, send_anything)
